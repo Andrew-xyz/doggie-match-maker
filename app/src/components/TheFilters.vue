f49f7ca78df6774831678d7e4d15f2d2f978fd8e
Andrew.xyz <andrew@tenty.xyz>
2023-03-31 10:57:55 -0500

    Filter Button Padding Issue

--- app/src/components/TheFilters.vue
+++ app/src/components/TheFilters.vue
@@ -90,7 +90,7 @@ function clearFilters() {
           </div>
         </div>
       </div>
-      <div class="flex items-center w-full h-fit px-4 pb-2">
+      <div class="flex items-center w-full h-fit px-4 py-2">
         <BaseButtonText
           class="w-full h-8 hover:bg-yellow-500/50 transition-all duration-300 border-2 border-purple-900"
           text="Clear Filters"
